1fab0b30a5fe8bcd80b67730cc99e31bab810c31
Isabella <bella.s.alves2009@gmail.com>
2025-11-18 18:48:53 -0300

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+document.querySelectorAll(".secao").forEach((secao) => {
+  observer.observe(secao);
+});
